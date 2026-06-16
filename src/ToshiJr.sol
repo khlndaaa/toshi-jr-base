fe511f900e18929891fb4a72d6af7f0e8e1dc256
khlndaaa <khlanovskiy@ukr.net>
2026-06-16 14:49:00 +0300

    feat: create src folder and ToshiJr.sol

---
+++ src/ToshiJr.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title Toshi Jr
+ * @notice ERC20 token deployed on Base
+ */
+contract HelloERC20 is ERC20 {
+    constructor() ERC20("Toshi Jr", "TOSHI JR") {}
+}commit message:
+feat: create src folder and ToshiJr.sol
